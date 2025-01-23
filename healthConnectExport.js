9fcd5dafa08be248b6988fb6214d104fbe9faa68
James Whayman <james@wildpress.co>
2025-01-23 23:25:30 +0000

    chore(js): unsplit fields

--- healthConnectExport.js
+++ healthConnectExport.js
@@ -1,6 +1,5 @@
 var jsonData = JSON.parse(aggregatedhealthdata);
 var entries = [];
-var regex = /^(\w+Record)_(\w+)$/;
 
 if (jsonData) {
   jsonData.forEach(function(data) {
@@ -11,12 +10,7 @@ if (jsonData) {
     Object.keys(data).forEach(function(key) {
       var value = data[key];
       if (excludedFields.indexOf(key) === -1 && value !== null && value !== undefined) {
-        var match = key.match(regex);
-        if (match) {
-          var recordType = match[1];
-          var measurement = match[2];
-          lines.push('healthConnect,recordType=' + recordType + ' ' + measurement + '=' + value + ' ' + timestamp);
-        }
+        lines.push('healthConnect,recordType=' + value + ' ' + timestamp);
       }
     });
     
